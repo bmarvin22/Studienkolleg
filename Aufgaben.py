b1ceec1309a916223b064fb2ad25b83458d57f7e
Maniragaba I. B. Marvin <misternova60@gmail.com>
2025-02-27 11:13:47 +0100

    updates

---
+++ Aufgaben.py
@@ -0,0 +1,14 @@
+meine_liste = [10, 20, 30, 40, 50]
+print(meine_liste)
+meine_liste2 = [1,2,3,4,5,]
+meine_liste2.append(6)
+meine_liste2.insert(0,0)
+meine_liste2.remove(3)
+print(meine_liste2)
+
+liste1 = [1,2,3,]
+liste2 = [4,5,6,]
+liste3 = liste1 + liste2
+print(liste3)
+
+meine_liste.index(40)
--- lesson3.py
+++ lesson3.py
@@ -1,5 +1,9 @@
-meine_liste = [1, 2, 3, 4, "Schule"]
+meine_liste = [1, 2, 3, 4, 'Schule']
 print(meine_liste)
 type(meine_liste)
-a = "hallo"
-type(a)
+#a = "hallo"
+#type(a)
+meine_liste[3] = 'Uni'
+print(meine_liste)
+meine_liste.append('Haus')
+print(meine_liste)
